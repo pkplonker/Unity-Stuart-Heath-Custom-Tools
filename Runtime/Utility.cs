0855a1f60677e51986c9cdaf1e492d6ca32904af
stuart <pkplonker@gmail.com>
2022-06-21 20:24:09 +0100

    Fix for KMB Func & added isnumbertype func

--- Runtime/Utility.cs
+++ Runtime/Utility.cs
@@ -2,6 +2,7 @@
 // Copyright (C) 2022 Stuart Heath. All rights reserved.
 //
 
+using System;
 using UnityEngine;
 
 namespace StuartHeathTools
@@ -25,7 +26,7 @@ namespace StuartHeathTools
 		{
 			if (amount >= 1000000000) return $"{amount / 1000000000}B";
 			if (amount >= 1000000) return $"{amount / 1000000}M";
-			if (amount >= 100000) return $"{amount / 100000}K";
+			if (amount >= 1000) return $"{amount / 100000}K";
 			return amount.ToString();
 		}
 
@@ -33,7 +34,7 @@ namespace StuartHeathTools
 		{
 			if (amount >= 1000000000) return $"{amount / 1000000000}B";
 			if (amount >= 1000000) return $"{amount / 1000000}M";
-			if (amount >= 100000) return $"{amount / 100000}K";
+			if (amount >= 1000) return $"{amount / 100000}K";
 			return amount.ToString();
 		}
 
@@ -41,7 +42,7 @@ namespace StuartHeathTools
 		{
 			if (amount >= 1000000000) return $"{amount / 1000000000}B";
 			if (amount >= 1000000) return $"{amount / 1000000}M";
-			if (amount >= 100000) return $"{amount / 100000}K";
+			if (amount >= 1000) return $"{amount / 100000}K";
 			return amount.ToString();
 		}
 
@@ -49,10 +50,31 @@ namespace StuartHeathTools
 		{
 			if (amount >= 1000000000) return $"{amount / 1000000000}B";
 			if (amount >= 1000000) return $"{amount / 1000000}M";
-			if (amount >= 100000) return $"{amount / 100000}K";
+			if (amount >= 1000) return $"{amount / 100000}K";
 			return amount.ToString();
 		}
 
 		#endregion
+
+		public static bool IsNumericType(this object o)
+		{
+			switch (Type.GetTypeCode(o.GetType()))
+			{
+				case TypeCode.Byte:
+				case TypeCode.SByte:
+				case TypeCode.UInt16:
+				case TypeCode.UInt32:
+				case TypeCode.UInt64:
+				case TypeCode.Int16:
+				case TypeCode.Int32:
+				case TypeCode.Int64:
+				case TypeCode.Decimal:
+				case TypeCode.Double:
+				case TypeCode.Single:
+					return true;
+				default:
+					return false;
+			}
+		}
 	}
 }
