466e7db5239534ac2e7a49414aa19d9aea1613c5
stuart <pkplonker@gmail.com>
2022-06-26 19:01:20 +0100

    Sofunc

--- Runtime/ScriptableObjectFuncs.cs
+++ Runtime/ScriptableObjectFuncs.cs
@@ -1,21 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
 public class ScriptableObjectFuncs : MonoBehaviour
 {
-    public static T[] GetAllInstances<T>() where T : ScriptableObject
-    {
-        var guids = AssetDatabase.FindAssets("t:"+ typeof(T).Name);  //FindAssets uses tags check documentation for more info
-        var a = new T[guids.Length];
-        for(var i =0;i<guids.Length;i++)         //probably could get optimized 
-        {
-            var path = AssetDatabase.GUIDToAssetPath(guids[i]);
-            a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
-        }
- 
-        return a;
- 
-    }
+	public static T[] GetAllInstances<T>() where T : ScriptableObject
+	{
+		var guids = AssetDatabase.FindAssets("t:" +
+		                                     typeof(T).Name); //FindAssets uses tags check documentation for more info
+		var a = new T[guids.Length];
+		for (var i = 0; i < guids.Length; i++) //probably could get optimized 
+		{
+			var path = AssetDatabase.GUIDToAssetPath(guids[i]);
+			a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+		}
+
+		return a;
+	}
 }
+#endif
